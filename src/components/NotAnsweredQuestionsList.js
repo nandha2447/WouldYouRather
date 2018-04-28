9c003b33b7ecd244d5731159fdfa1361ba949dc5
Nandha Kumar <nandha2447@gmail.com>
2018-04-28 12:24:56 +0530

    Linked to individual question page. Added individual images

--- src/components/NotAnsweredQuestionsList.js
+++ src/components/NotAnsweredQuestionsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {handleSaveAnswer} from '../actions/answers'
+import {Link} from 'react-router-dom'
 class NotAnsweredQuestionsList extends React.Component{
     onSelectAnswer(qid,answer){
         console.log();
@@ -14,29 +15,31 @@ class NotAnsweredQuestionsList extends React.Component{
     render(){
         return (
             <div>
-        <h3>This is not answered questions list </h3>
-        <ul>
-        {this.props.authedUserUnAnsweredQuestions.map(question => (
-        <li key={question.id}>
-            <div>
-                <h3> Would you rather </h3>
-                <div onClick={()=>{this.onSelectAnswer(question.id,'optionOne')}}>
-                    <h4>{question.optionOne.text}</h4>
+            <h3>This is not answered questions list </h3>
+            <ul>
+            {this.props.authedUserUnAnsweredQuestions.map(question => (
+            <li key={question.id}>
+                <div>
+                    <h3> Would you rather </h3>
+                    <img src={this.props.users[question.author].avatarURL}/>
+                    <Link to={`questions/${question.id}`} onClick={()=>{this.onSelectAnswer(question.id,'optionOne')}}>
+                        <h4>{question.optionOne.text}</h4>
+                    </Link>
+                    <Link to={`questions/${question.id}`} onClick={()=>{this.onSelectAnswer(question.id,'optionTwo')}}>
+                        <h4>{question.optionTwo.text}</h4>
+                    </Link>
                 </div>
-                <div onClick={()=>{this.onSelectAnswer(question.id,'optionTwo')}}>
-                    <h4>{question.optionTwo.text}</h4>
-                </div>
-            </div>
-        </li>))}   
-        </ul>
-    </div>
+            </li>))}   
+            </ul>
+        </div>
         )
     }
 }
 
 function mapStateToProps(state){
     return {
-        authedUser: state.authedUser
+        authedUser: state.authedUser,
+        users: state.users
     }
 }
 
